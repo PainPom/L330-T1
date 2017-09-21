f92ddd591b8f138c8249e379520df4d5c94d0bbe
Michaël Gendron-Renaud <michaelgendronrenaud@gmail.com>
2017-09-21 11:38:55 -0400

    Ajout des commentaires et de la classe EcartType pour la derniere fonction mathématique a couvrir

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,43 @@
-
+/**
+ * Classe responsable de l'affichage des résultats pour les différents calculs statistiques
+ * 
+ * @author Michael Gendron-Renaud
+ *
+ */
 public class Main {
 
+	//Attribut regroupant toutes les données considérées dans les calculs
 	private static int[] _data = {186,699,132,272,291,331,199,1890,788,1601};
 	
+	/**
+	 * Méthode principale servant a l'affichage des résultats des calculs de moyenne, distance, somme des distances,
+	 * variance et écart-type
+	 * @param String[] args : les arguments du main s'il y en a
+	 */
 	public static void main(String[] args) {
 		
+		//Calcul d'une moyenne par un attribut de la classe Moyenne et affichage
 		Moyenne moyenne = new Moyenne(_data);
 		System.out.println("Moyenne : " + moyenne.getMean());
 		
+		//Calcul d'une distance pour toutes les valeurs par un attribut de la classe Distance 
+		//puis affichage
 		Distance distance = new Distance(_data, moyenne);
 		for(int i = 0; i < distance.getDistanceGrid().length; i++)
 			System.out.println("Distance " + i + " : " + distance.getDistanceAt(i));
 
+		//Calcul d'une somme des distances par un attribut de la classe Distance puis affichage
 		System.out.println("Somme des distances : " + distance.getDistanceSum());
 		
+		//Calcul d'une variance par un attribut de la classe Variance 
+		//et affichage
 		Variance variance = new Variance(distance);
 		System.out.println(variance.getVariance());
 	
+		//Calcul de l'écart-type par un attribut de la classe EcartType et affichage
+		EcartType ecartType = new EcartType(variance);
+		System.out.println(ecartType.getEcartType());
+		
 	}
 	
 }
--- src/Variance.java
+++ src/Variance.java
@@ -1,29 +1,54 @@
-
+/**
+ * Classe responsable de calculer la variance a partir de données calculées préalablement via les autres classes
+ * de calculs statistiques
+ * 
+ * @author Michael Gendron-Renaud
+ *
+ */
 public class Variance {
 
+	//Attributs
 	private double _variance;
 	private double _length;
 	
+	//Constructeur
 	public Variance(Distance distance) {
 		
+		//Initialisation des attributs
 		_variance = 0;		
 		_length = distance.getDistanceGrid().length;
 		
+		//Mise a jour des attributs
 		_variance = calculateVariance(distance);
 				
 	}
 	
+	/**
+	 * Méthode responsable de calculer la variance a partir d'une distance fournie
+	 * 
+	 * @param Distance distance : la distance calculée préalablement
+	 * @return double _localVariance : le résultat du calcul de la variance
+	 */
 	public double calculateVariance(Distance distance) {
 		
+		//Initialisation
 		double _localVariance = 0;
 		
+		//Calcul
 		_localVariance = (1/(_length - 1))*distance.getDistanceSum();
 		
+		//Retour
 		return _localVariance;
 		
 	}
 	
+	/**
+	 * Accesseur permettant d'aller chercher la valeur de la variance en étant a l'extérieur de la classe
+	 * 
+	 * @return double _variance : l'attribut de la variance
+	 */
 	public double getVariance() {
+		//Retour
 		return _variance;
 	}
 	
